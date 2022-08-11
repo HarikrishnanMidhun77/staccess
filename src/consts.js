33c692e2e272bd1a0d24d3a0ec5e34638eabe88d
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2022-08-10 20:55:58 -0400

    fontFuntions change 5

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
 export const cdnLink =
-  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@698a380af11aaf8d6a7b011d4bdc278b6b99e12c/";
+  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@db6d23c8ed4c7de85fef7db243d909d343dc6031/";
