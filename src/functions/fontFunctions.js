a9ab885ef2daf8ededb29a1b446271e9b42f41f9
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2022-08-10 21:08:58 -0400

    fontFuntions change 7

--- src/functions/fontFunctions.js
+++ src/functions/fontFunctions.js
@@ -6,7 +6,7 @@ const hover__ = "#5e91f2";
 const selected__ = "#003b8e";
 //import { cdnLink } from "../consts.js";
 const cdnLink =
-  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@db6d23c8ed4c7de85fef7db243d909d343dc6031/";
+  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@088d6aaf8a8a9f9bef95c23a61e2c5a6c2d2b47c/";
 
 function getCookie(cname) {
   let name = cname + "=";
