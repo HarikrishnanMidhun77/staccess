5f46cf93eb735a1718ae6a217344d73bc6c610d5
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2022-09-07 17:52:54 -0400

    5 more tools in fd5d99e879e03ffd8765

--- dist/fd5d99e879e03ffd8765.js
+++ dist/fd5d99e879e03ffd8765.js
@@ -17,6 +17,11 @@ var letter_spacing_count = 0,
 var line_height_count = 0,
   line_height_direction = 1;
 var line_focus_flag = false;
+var reading_ruler_flag = false;
+var white_cursor_flag = false;
+var bluelight_filter_flag = false;
+var screen_overlay_flag = false;
+var dyslexia_ruler_flag = false;
 
 function getCookie(cname) {
   let name = cname + "=";
@@ -369,4 +374,360 @@ function applyLineFocus() {
     }
   }
 }
-function applyReadingRuler() {}
+function applyReadingRuler() {
+  if (!reading_ruler_flag) {
+    document.getElementById("staccess__readingruler__btn").style.background =
+      selected__;
+    reading_ruler_flag = true;
+    let style = document.createElement("style");
+    style.innerHTML = `#ta-a11y-works-widget-reading-ruler-div {
+        position: fixed;
+        width: 100%;
+        height: 10px;
+        z-index: 999999;
+        background:black;
+        outline: 3px solid #cece05;
+        pointer-events: none;
+      }`;
+
+    let shadowDiv = document.createElement("div");
+    shadowDiv.id = "ta-a11y-works-widget-shadow-root";
+    let shadowRoot = shadowDiv.attachShadow({ mode: "open" });
+    let shadowChild = document.createElement("div");
+    shadowChild.classList.add("ta-a11y-works-widget-shadow-content");
+    let script = document.createElement("script");
+    script.textContent = `
+    var readingRuler = document.querySelector("#ta-a11y-works-widget-shadow-root").shadowRoot.querySelector("#ta-a11y-works-widget-reading-ruler-div");
+    document.body.addEventListener("mousemove", (event) => {
+        let mouseY = event.clientY +
+        (document && document.scrollTop  || document.body && document.body.scrollTop  || 0) -
+        (document && document.clientTop  || document.body && document.body.clientTop  || 0 );
+        let currentScale = parseFloat(window.getComputedStyle(document.body).zoom);
+        mouseY = mouseY/Number(currentScale)+15;
+        readingRuler.style.top = mouseY+"px";
+    })
+    `;
+
+    let readingRuler = document.createElement("div");
+    readingRuler.id = "ta-a11y-works-widget-reading-ruler-div";
+
+    let wrapperDiv = document.createElement("div");
+    wrapperDiv.id = "ta-a11y-works-widget-reading-ruler-wrapper";
+    wrapperDiv.appendChild(script);
+    wrapperDiv.appendChild(style);
+    wrapperDiv.appendChild(readingRuler);
+    shadowChild.appendChild(wrapperDiv);
+    shadowRoot.appendChild(shadowChild);
+
+    if (document.querySelector("#ta-a11y-works-widget-shadow-root")) {
+      document
+        .querySelector("#ta-a11y-works-widget-shadow-root")
+        .shadowRoot.appendChild(wrapperDiv);
+    } else {
+      document.body.appendChild(shadowDiv);
+    }
+  } else {
+    document.getElementById("staccess__readingruler__btn").style.background =
+      primary__;
+    reading_ruler_flag = false;
+    var shadow = document.querySelector("#ta-a11y-works-widget-shadow-root");
+    if (shadow) {
+      var readingRulerDiv = shadow.shadowRoot.querySelectorAll(
+        "#ta-a11y-works-widget-reading-ruler-wrapper"
+      );
+      readingRulerDiv.forEach((item) => item.remove());
+    }
+  }
+}
+function applyWhiteCursor() {
+  if (!white_cursor_flag) {
+    document.getElementById("staccess__whitecursor__btn").style.background =
+      selected__;
+    white_cursor_flag = true;
+    var style = document.createElement("style");
+    style.id = "ta-a11y-works-widget-white-mouse";
+    // var cursor = chrome.extension.getURL("icons/whiteCursor32.png");
+    // var hover = chrome.extension.getURL("icons/whiteHover32.png");
+
+    var cursor = cdnLink + "src/icons/whiteCursor32.png";
+    var hover = cdnLink + "src/icons/whiteHover32.png";
+    style.innerHTML = `body *{cursor: url(${cursor}), auto !important;} a:hover, button:hover {cursor: url(${hover}), auto !important;}`;
+    document.head.appendChild(style);
+  } else {
+    document.getElementById("staccess__whitecursor__btn").style.background =
+      primary__;
+    white_cursor_flag = false;
+    let styleTag = document.head.querySelector(
+      "#ta-a11y-works-widget-white-mouse"
+    );
+    if (styleTag) styleTag.remove();
+  }
+}
+function applyBluelightFilter() {
+  if (!bluelight_filter_flag) {
+    document.getElementById("staccess__bluelightfilter__btn").style.background =
+      selected__;
+    bluelight_filter_flag = true;
+
+    // let styleTag = document.querySelector("#ta_bluelight");
+
+    let style = document.createElement("style");
+    style.innerHTML = `
+    #ta_bluelight {
+      opacity: 0.2 !important;
+      background-color: #ffffbf !important;
+      width: 100% !important;
+      height: 100% !important;
+      z-index: 9999 !important;
+      top: 0 !important;
+      left: 0 !important;
+      position: fixed !important;
+      pointer-events: none !important;
+    }
+    `;
+    style.id = "ta-a11y-works-widget-blue-light";
+    document.head.appendChild(style);
+
+    var d = document.createElement("div");
+    d.id = "ta_bluelight";
+    document.body.insertBefore(d, document.body.childNodes[0]);
+    // var d = document.createElement("div");
+    // d.id = "ta_bluelight";
+    // document.body.insertBefore(d, document.body.childNodes[0]);
+  } else {
+    document.getElementById("staccess__bluelightfilter__btn").style.background =
+      primary__;
+    bluelight_filter_flag = false;
+    let styleTag = document.querySelector("#ta_bluelight");
+    if (styleTag) styleTag.remove();
+  }
+}
+function applyScreenOverlay() {
+  if (!screen_overlay_flag) {
+    document.getElementById("staccess__screenoverlay__btn").style.background =
+      selected__;
+    screen_overlay_flag = true;
+
+    // document
+    //   .querySelectorAll("#ta-a11y-works-widget-shadow-root")
+    //   .forEach((item, key) => {
+    //     item.shadowRoot
+    //       .querySelector("#ta-a11y-widget-screen-overlay-wrapper")
+    //       .remove();
+    //   });
+    var checkExist = setInterval(function () {
+      if (document.body) {
+        // console.log("body tag loaded");
+        var style = document.createElement("style");
+        style.id = "ta-a11y-widget-screen-overlay";
+        style.innerHTML = `.ta-a11y-widget-screen-overlay {  
+              position: fixed;
+              width: 100%;
+              background-color: rgb(10 10 10 / 50%);
+              z-index: 999999;
+              top:0px;
+              left:0;
+              height:100%;
+              opacity:0.5;
+              pointer-events:none;
+              z-index:999990
+          }`;
+        document.head.appendChild(style);
+
+        let shadowDiv = document.createElement("div");
+        shadowDiv.id = "ta-a11y-works-widget-shadow-root";
+        let shadowRoot = shadowDiv.attachShadow({ mode: "open" });
+        let shadowChild = document.createElement("div");
+        shadowChild.classList.add("ta-a11y-works-widget-shadow-content");
+
+        let overlayDiv = document.createElement("div");
+        overlayDiv.classList.add("ta-a11y-widget-screen-overlay");
+        // overlayDiv.style.height='100%';
+        let wrapperDiv = document.createElement("div");
+        wrapperDiv.id = "ta-a11y-widget-screen-overlay-wrapper";
+        wrapperDiv.appendChild(style);
+        wrapperDiv.appendChild(overlayDiv);
+        shadowChild.appendChild(wrapperDiv);
+        shadowRoot.appendChild(shadowChild);
+        if (document.querySelector("#ta-a11y-works-widget-shadow-root")) {
+          document
+            .querySelector("#ta-a11y-works-widget-shadow-root")
+            .shadowRoot.appendChild(wrapperDiv);
+        } else {
+          document.body.appendChild(shadowDiv);
+        }
+
+        clearInterval(checkExist);
+      }
+    }, 10);
+  } else {
+    document.getElementById("staccess__screenoverlay__btn").style.background =
+      primary__;
+    screen_overlay_flag = false;
+
+    var overlays = document.body
+      .querySelector("#ta-a11y-works-widget-shadow-root")
+      .shadowRoot.querySelectorAll("#ta-a11y-widget-screen-overlay-wrapper");
+    if (overlays.length > 0) {
+      overlays.forEach((item, key) => {
+        item.remove();
+      });
+    }
+  }
+}
+function applyDyslexiaRuler() {
+  if (!dyslexia_ruler_flag) {
+    document.getElementById("staccess__dyslexiaruler__btn").style.background =
+      selected__;
+    dyslexia_ruler_flag = true;
+
+    var shadow = document.querySelector("#ta-a11y-works-widget-shadow-root");
+    if (shadow) {
+      let dyslexiaRuler = shadow.shadowRoot.querySelectorAll(
+        "#ta-a11y-works-widget-dyslexia-ruler-wrapper"
+      );
+      dyslexiaRuler.forEach((item) => item.remove());
+    }
+    // document.querySelectorAll("#ta-a11y-works-widget-shadow-root").forEach((item, key) => {
+    //   item.shadowRoot.querySelector("#ta-a11y-works-widget-dyslexia-ruler-wrapper").remove();
+    // });
+
+    let style = document.createElement("style");
+    style.innerHTML = `#ta-a11y-works-widget-dyslexia-ruler-div {
+        position: fixed;
+        width: 98%;
+        height: 200px;
+        z-index: 999999;
+        pointer-events: none;
+        margin:0px auto;
+        right:1%
+      }
+      // #ta-a11y-widget-dyslexia-ruler-top{
+      //   height:40px;
+      //   z-index:999999 !important;
+      //   pointer-events:none;
+      //   background-color:rgb(233 0 197);
+      //   border-top-left-radius: 5px;
+      //   border-top-right-radius: 5px;
+      //   opacity:50%
+      // }
+      #ta-a11y-widget-dyslexia-ruler-middle{
+        height:100px;
+        z-index:999999 !important;
+        pointer-events:none;
+        background-color:#000084;
+
+      }
+      #ta-a11y-widget-dyslexia-ruler-bottom{
+        height:50px;
+        z-index:999999 !important;
+        pointer-events:none;
+        background-color:rgb(233 0 197);
+        border-bottom-left-radius: 5px;
+        border-bottom-right-radius: 5px;
+        opacity:50%
+      }
+      
+      `;
+
+    let shadowDiv = document.createElement("div");
+    shadowDiv.id = "ta-a11y-works-widget-shadow-root";
+    let shadowRoot = shadowDiv.attachShadow({ mode: "open" });
+    let shadowChild = document.createElement("div");
+    shadowChild.classList.add("ta-a11y-works-widget-shadow-content");
+    let script = document.createElement("script");
+    script.textContent = `
+    var dyslexiaRuler = document.querySelector("#ta-a11y-works-widget-shadow-root").shadowRoot.querySelector("#ta-a11y-works-widget-dyslexia-ruler-div");
+    document.body.addEventListener("mousemove", mouseMoveListener);
+
+    function mouseMoveListener(event){
+      let rulerDiv = document.querySelector("#ta-a11y-works-widget-shadow-root").shadowRoot.querySelector("#ta-a11y-works-widget-dyslexia-ruler-div");
+      if(rulerDiv){
+        let mouseY = event.clientY +
+        (document && document.scrollTop  || document.body && document.body.scrollTop  || 0) -
+        (document && document.clientTop  || document.body && document.body.clientTop  || 0 );
+        let currentScale = parseFloat(window.getComputedStyle(document.body).zoom);
+        mouseY = mouseY/Number(currentScale);
+        var center = (document.querySelector("#ta-a11y-works-widget-shadow-root").shadowRoot.querySelector("#ta-a11y-widget-dyslexia-ruler-bottom").offsetHeight)/2;
+        mouseY -=100+center;
+        dyslexiaRuler.style.top = mouseY+"px";
+      }
+    }
+    // document.body.addEventListener("mousemove", (event) => {
+    //     let mouseY = event.clientY +
+    //     (document && document.scrollTop  || document.body && document.body.scrollTop  || 0) -
+    //     (document && document.clientTop  || document.body && document.body.clientTop  || 0 );
+    //     let currentScale = parseFloat(window.getComputedStyle(document.body).zoom);
+    //     mouseY = mouseY/Number(currentScale);
+    //     mouseY -=(document.querySelector("#ta-a11y-works-widget-shadow-root").shadowRoot.querySelector("#ta-a11y-widget-dyslexia-ruler-top").offsetHeight)/2;
+    //     dyslexiaRuler.style.top = mouseY+"px";
+    // })
+    `;
+
+    let dyslexiaRuler = document.createElement("div");
+    dyslexiaRuler.id = "ta-a11y-works-widget-dyslexia-ruler-div";
+
+    // let topDiv = document.createElement("div");
+    // topDiv.id = "ta-a11y-widget-dyslexia-ruler-top";
+    let middleDiv = document.createElement("div");
+    middleDiv.id = "ta-a11y-widget-dyslexia-ruler-middle";
+    let bottomDiv = document.createElement("div");
+    bottomDiv.id = "ta-a11y-widget-dyslexia-ruler-bottom";
+
+    // dyslexiaRuler.appendChild(topDiv);
+    dyslexiaRuler.appendChild(middleDiv);
+    dyslexiaRuler.appendChild(bottomDiv);
+
+    let wrapperDiv = document.createElement("div");
+    wrapperDiv.id = "ta-a11y-works-widget-dyslexia-ruler-wrapper";
+    wrapperDiv.appendChild(script);
+    wrapperDiv.appendChild(style);
+    wrapperDiv.appendChild(dyslexiaRuler);
+    shadowChild.appendChild(wrapperDiv);
+    shadowRoot.appendChild(shadowChild);
+
+    if (document.querySelector("#ta-a11y-works-widget-shadow-root")) {
+      document
+        .querySelector("#ta-a11y-works-widget-shadow-root")
+        .shadowRoot.appendChild(wrapperDiv);
+    } else {
+      document.body.appendChild(shadowDiv);
+    }
+  } else {
+    document.getElementById("staccess__dyslexiaruler__btn").style.background =
+      primary__;
+    dyslexia_ruler_flag = false;
+
+    document.body.removeEventListener("mousemove", mouseMoveListener);
+    function mouseMoveListener(event) {
+      let mouseY =
+        event.clientY +
+        ((document && document.scrollTop) ||
+          (document.body && document.body.scrollTop) ||
+          0) -
+        ((document && document.clientTop) ||
+          (document.body && document.body.clientTop) ||
+          0);
+      let currentScale = parseFloat(
+        window.getComputedStyle(document.body).zoom
+      );
+      mouseY = mouseY / Number(currentScale);
+      mouseY -=
+        document
+          .querySelector("#ta-a11y-works-widget-shadow-root")
+          .shadowRoot.querySelector("#ta-a11y-widget-dyslexia-ruler-top")
+          .offsetHeight / 2;
+      dyslexiaRuler.style.top = mouseY + "px";
+    }
+
+    var shadow = document.body.querySelector(
+      "#ta-a11y-works-widget-shadow-root"
+    );
+    if (shadow) {
+      let dyslexiaRulerDiv = shadow.shadowRoot.querySelectorAll(
+        "#ta-a11y-works-widget-dyslexia-ruler-wrapper"
+      );
+      dyslexiaRulerDiv.forEach((item) => item.remove());
+    }
+  }
+}
