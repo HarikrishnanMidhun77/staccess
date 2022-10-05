5fcd01a5d511abe7f73c5bcfbd01275ae5f50a81
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2022-10-04 21:34:57 -0400

    ON button error fix

--- dist/fd5d99e879e03ffd8765.js
+++ dist/fd5d99e879e03ffd8765.js
@@ -7,7 +7,7 @@ const hover__ = "#5e91f2";
 const selected__ = "#003b8e";
 //import { cdnLink } from "../consts.js";
 const cdnLink =
-  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@1fe596799f7dd5e445a3f4cb62f5646f40fd92f9/";
+  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@5a415f8d4c053cde4bd2b9831e80d59e67652cb9/";
 
 var dyslexiaFont__flag = false;
 var font_size_count = 0,
