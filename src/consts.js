679da34800be942aff2e9232f1297d64a8a16e7a
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2022-10-04 21:30:23 -0400

    Profiling Done Server 2

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
 export const cdnLink =
-  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@1fe596799f7dd5e445a3f4cb62f5646f40fd92f9/";
+  "https://cdn.jsdelivr.net/gh/HarikrishnanMidhun77/staccess@5a415f8d4c053cde4bd2b9831e80d59e67652cb9/";
